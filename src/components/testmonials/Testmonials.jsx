918fef5005815913ccde96ddb2655f37ca42a39e
oshadaera68 <mindartlk010@gmail.com>
2023-02-25 20:57:38 +0530

    testmonial part finished

--- src/components/testmonials/Testmonials.jsx
+++ src/components/testmonials/Testmonials.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import './testmonials.css'
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import Image1 from '../../assets/avatar-1.svg'
 import Image3 from '../../assets/avatar-3.svg'
+
+// swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const data = [
   {
@@ -27,9 +35,27 @@ const Testmonials = () => {
     <section className="testmonials container section">
       <h2 className="section-title">Clients & Reviews</h2>
 
-      <div className="testmonials__container grid">
-      
-      </div>
+      <Swiper className="testmonials__container grid"
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        grabCursor={true}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ id, image, title, subtitle, comment }) => {
+          return (
+            <SwiperSlide className="testmonial__item" key={id}>
+              <div className="thumb">
+                <img src={image} alt="" />
+              </div>
+              <h3 className="testmonials__title">{title}</h3>
+              <span className="subtitle">{subtitle}</span>
+              <div className="comment">{comment}</div>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
     </section>
   )
 }
